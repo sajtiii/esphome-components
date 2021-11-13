a5f33f6c0e53f57d33f1d008c5be92f1027bbb7d
Attila Tamás Nagy <nagyattila949@gmail.com>
2021-11-13 18:55:51 +0100

    Sensor setup

--- components/apds9306/apds9306.cpp
+++ components/apds9306/apds9306.cpp
@@ -6,6 +6,55 @@ namespace apds9306 {
 
 static const char *const TAG = "apds9306";
 
+void APDS9306Component::power_on() {
+    if (this->write_byte(APDS9306_CMD_MAIN_CTRL, APDS9306_MAIN_CTRL_ENABLE)) {
+        this->mark_failed();
+    }
+}
+
+void APDS9306Component::power_off() {
+    if (this->write_byte(APDS9306_CMD_MAIN_CTRL, APDS9306_MAIN_CTRL_DISABLE)) {
+        this->mark_failed();
+    }
+}
+
+int APDS9306Component::meas_rate_value() {
+    switch (this->meas_rate) {
+        case APDS9306_ALS_MEAS_RES_20BIT_400MS:
+            return 400;
+        case APDS9306_ALS_MEAS_RES_19BIT_200MS:
+            return 200;
+        case APDS9306_ALS_MEAS_RES_18BIT_100MS:
+            return 100;
+        case APDS9306_ALS_MEAS_RES_17BIT_50MS:
+            return 50;
+        case APDS9306_ALS_MEAS_RES_16BIT_25MS:
+            return 25;
+        case APDS9306_ALS_MEAS_RES_13BIT_3MS:
+            return 3;
+        default:
+            return -1;
+    }
+}
+
+int APDS9306Component::gain_value() {
+    switch (this->gain) {
+        case APDS9306_ALS_GAIN_1:
+            return 1;
+        case APDS9306_ALS_GAIN_3:
+            return 3;
+        case APDS9306_ALS_GAIN_6:
+            return 6;
+        case APDS9306_ALS_GAIN_9:
+            return 9;
+        case APDS9306_ALS_GAIN_18:
+            return 18;
+        default:
+            return -1.0;
+    }
+}
+
+
 void APDS9306Component::setup() {
     ESP_LOGCONFIG(TAG, "Setting up APDS9306...");
     uint8_t part_id = 0;
@@ -14,69 +63,41 @@ void APDS9306Component::setup() {
         return;
     }
 
-    if (part_id != 0xB1 && part_id != 0xB3) {
+    if (part_id != APDS9306_PART_ID_APDS9306 && part_id != APDS9306_PART_ID_APDS9306065) {
         ESP_LOGE(TAG, "Wrong chip type...");
         this->mark_failed();
         return;
     }
 
-    ESP_LOGD(TAG, "    Part ID: APDS-9306%s", part_id == 0XB3 ? "-065" : "");
+    ESP_LOGD(TAG, "    Part ID: APDS-9306%s", part_id == APDS9306_PART_ID_APDS9306065 ? "-065" : "");
+    this->power_off();
+}
+
+void APDS9306Component::set_measurement_rate(APDS9306_ALS_MEAS_RATE meas_rate) {
+    this->meas_rate = meas_rate;
+    if (!this->write_byte(APDS9306_CMD_ALS_MEAS_RATE, meas_rate)) {
+        this->mark_failed();
+    }
+}
+
+void APDS9306Component::set_gain(APDS9306_ALS_GAIN gain) {
+    this->gain = gain;
+    if (!this->write_byte(APDS9306_CMD_ALS_GAIN, gain)) {
+        this->mark_failed();
+    }
 }
 
 void APDS9306Component::update() {
-  publish_state(45);
- // if (this->status_has_warning()) {
- //   ESP_LOGD(TAG, "Retrying to reconnect the sensor.");
- //   this->write_command_(SHT3XD_COMMAND_SOFT_RESET);
- // }
- // if (!this->write_command_(SHT3XD_COMMAND_POLLING_H)) {
- //   this->status_set_warning();
- //   return;
- // }
+    this->power_on();
+    while (!(this->read_byte(APDS9306_CMD_MAIN_STATUS) && APDS9306_MAIN_STATUS_ALS_DATA));
+    uint32_t raw_data = this->read_byte(APDS9306_CMD_ALS_DATA_2) << 16;
+    raw_data |= this->read_byte(APDS9306_CMD_ALS_DATA_2) << 8;
+    raw_data |= this->read_byte(APDS9306_CMD_ALS_DATA_2);
+    this->power_off();
 
- // this->set_timeout(50, [this]() {
- //   uint16_t raw_data[2];
- //   if (!this->read_data_(raw_data, 2)) {
- //     this->status_set_warning();
- //     return;
- //   }
-
- //   float temperature = 175.0f * float(raw_data[0]) / 65535.0f - 45.0f;
- //   float humidity = 100.0f * float(raw_data[1]) / 65535.0f;
-
- //   ESP_LOGD(TAG, "Got temperature=%.2f°C humidity=%.2f%%", temperature, humidity);
- //   if (this->temperature_sensor_ != nullptr)
- //     this->temperature_sensor_->publish_state(temperature);
- //   if (this->humidity_sensor_ != nullptr)
- //     this->humidity_sensor_->publish_state(humidity);
- //   this->status_clear_warning();
- // });
+    publish_state(((float)raw_data / this->gain_value()) * (100.0 / this->meas_rate_value()));
 }
 
-// bool APDS9306Component::write_command_(uint8_t command) {
-//     return this->write_byte(command);
-// }
 
-//bool APDS9306Component::read_data_(uint16_t *data, uint8_t len) {
- /*const uint8_t num_bytes = len * 3;
- std::vector<uint8_t> buf(num_bytes);
-
- if (this->read(buf.data(), num_bytes) != i2c::ERROR_OK) {
-   return false;
- }
-
- for (uint8_t i = 0; i < len; i++) {
-   const uint8_t j = 3 * i;
-   uint8_t crc = sht_crc(buf[j], buf[j + 1]);
-   if (crc != buf[j + 2]) {
-     ESP_LOGE(TAG, "CRC8 Checksum invalid! 0x%02X != 0x%02X", buf[j + 2], crc);
-     return false;
-   }
-   data[i] = (buf[j] << 8) | buf[j + 1];
- }
-
- return true;*/
-//}
-
-}  // namespace sht3xd
+}  // namespace apds9306
 }  // namespace esphome
--- components/apds9306/sensor.py
+++ components/apds9306/sensor.py
@@ -6,6 +6,8 @@ from esphome.const import (
     ICON_LIGHTBULB,
     DEVICE_CLASS_ILLUMINANCE,
     STATE_CLASS_MEASUREMENT,
+    CONF_RATE,
+    CONF_GAIN,
 )
 
 DEPENDENCIES = ["i2c"]
@@ -17,6 +19,27 @@ APDS9306Component = apds9306.class_(
     "APDS9306Component", sensor.Sensor, cg.PollingComponent, i2c.I2CDevice
 )
 
+APDS9306AlsMeasRate = apds9306.enum("APDS9306_ALS_MEAS_RATE")
+ALS_MEAS_RATE_OPTIONS = {
+    "25MS": ADPS9306AlsMeasRate.APDS9306_ALS_MEAS_RATE_25MS,
+    "50MS": ADPS9306AlsMeasRate.APDS9306_ALS_MEAS_RATE_50MS,
+    "100MS": ADPS9306AlsMeasRate.APDS9306_ALS_MEAS_RATE_100MS,
+    "200MS": ADPS9306AlsMeasRate.APDS9306_ALS_MEAS_RATE_200MS,
+    "500MS": ADPS9306AlsMeasRate.APDS9306_ALS_MEAS_RATE_500MS,
+    "1000MS": ADPS9306AlsMeasRate.APDS9306_ALS_MEAS_RATE_1000MS,
+    "2000MS": ADPS9306AlsMeasRate.APDS9306_ALS_MEAS_RATE_2000MS,
+}
+
+APDS9306AlsGain = apds9306.enum("APDS9306_ALS_GAIN")
+ALS_GAIN_OPTIONS = {
+    "1X": ADP9306AlsGain.APDS9306_ALS_GAIN_1,
+    "3X": ADP9306AlsGain.APDS9306_ALS_GAIN_3,
+    "6X": ADP9306AlsGain.APDS9306_ALS_GAIN_6,
+    "9X": ADP9306AlsGain.APDS9306_ALS_GAIN_9,
+    "18X": ADP9306AlsGain.APDS9306_ALS_GAIN_18,
+}
+
+
 CONFIG_SCHEMA = (
     sensor.sensor_schema(
         icon=ICON_LIGHTBULB,
@@ -27,6 +50,12 @@ CONFIG_SCHEMA = (
     .extend(
         {
             cv.GenerateID(): cv.declare_id(APDS9306Component),
+            cv.Optional(CONF_RATE, default="100MS"): cv.enum(
+                ALS_MEAS_RATE_OPTIONS, upper=True
+            ),
+            cv.Optional(CONF_GAIN, default="1X"): cv.enum(
+                ALS_GAIN_OPTIONS, upper=True
+            ),
         }
     )
     .extend(cv.polling_component_schema("60s"))
@@ -39,3 +68,5 @@ async def to_code(config):
     await cg.register_component(var, config)
     await i2c.register_i2c_device(var, config)
     await sensor.register_sensor(var, config)
+    cg.add(var.set_measurement_rate(config[CONF_RATE]))
+    cg.add(var.set_gain(config[CONF_GAIN]))
